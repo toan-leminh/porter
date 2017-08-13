2a0e08ae389c914b9f598c9bae51eb3d050be610
Le Minh Toan <toan.lm@briswell.com>
2017-08-13 23:42:56 +0900

    Change Top page

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -8,7 +8,7 @@
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
             </button>
-            <a class="navbar-brand" href="{{ route('home') }}">My Transporter</a>
+            <a class="navbar-brand" href="{{ route('top') }}">My Transporter</a>
         </div>
         <!-- Collect the nav links, forms, and other content for toggling -->
         <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
